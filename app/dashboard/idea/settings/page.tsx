80d30b3e2bee1fa4dcd2de1e59f59215a36fc020
Kingsley <kingsobino@gmail.com>
2023-12-31 10:08:28 +0100

    improve journey interface

--- app/dashboard/idea/settings/page.tsx
+++ app/dashboard/idea/settings/page.tsx
@@ -1,51 +1,111 @@
 "use client"
-import { Card, CardBody, Textarea } from "@nextui-org/react";
+import { CommentIcon } from "@/components/icons/CommentIcon";
+import { Button, Card, CardBody, CardFooter, CardHeader, Textarea } from "@nextui-org/react";
 import { useState } from "react";
 
-function StepCard({ expanded = false, indicator = true }: { expanded: boolean, indicator: boolean }) {
+function StepCard({ expanded = false, indicator = true, styles = "" }: { expanded: boolean, indicator: boolean, styles: string }) {
   const [xpanded, setXpanded] = useState(expanded)
   return (
-    <div className="relative cursor-pointer" onClick={() => setXpanded(!xpanded)}>
+    <div className={`relative cursor-pointer max-w-2xl pb-4 ${styles}`} onClick={() => setXpanded(!xpanded)}>
       <Card>
         <CardBody>
-          <p className={`${ xpanded ? 'h-24' : ''}`}>Start (Idea)</p>
+          <p className={`${xpanded ? 'h-24' : ''}`}>Start (Idea)</p>
         </CardBody>
       </Card>
-      { indicator && <div className="absolute h-10 w-4 border-dotted border-l-2 border-b-2 left-12"></div> }
+      {indicator && <div className="absolute h-10 w-4 border-dotted border-l-2 border-b-2 left-12"></div>}
     </div>
   )
 }
 
 export default function Settings() {
   return (
-    <div className="p-6 pt-10">
-      <div className="flex flex-col gap-10">
-        <div>
-          <Textarea
-            label="When have you reached product market fit?"
-            labelPlacement="outside"
+    <div className="p-6 pt-10 flex flex-col gap-8">
+      <Card>
+        <CardHeader>
+          <div className="p-6 w-full">
+            <Textarea
+              label="When have you reached product market fit?"
+              labelPlacement="outside"
 
-            className="max-w-full"
-          />
-        </div>
-
-        <div>
-          <div>
-            <StepCard expanded={false} indicator />
-          </div>
-          <div className="relative top-4 left-16">
-            <StepCard expanded indicator />
+              className="max-w-full"
+            />
           </div>
 
-          <div className="relative top-8 left-32">
-            <StepCard expanded={false} indicator />
+        </CardHeader>
+        <CardBody>
+
+          <div className="py-3 pl-10">
+            <StepCard expanded={false} indicator styles="" />
+
+
+            <StepCard expanded indicator styles="relative  left-16 w-[calc(100%-64px)]" />
+
+
+
+            <StepCard expanded={false} indicator styles="relative left-32 w-[calc(100%-128px)]" />
+
+
+
+            <StepCard expanded={false} indicator={false} styles="relative left-48 w-[calc(100%-192px)]" />
           </div>
 
-          <div className="relative top-12 left-48">
-            <StepCard expanded={false} indicator={false} />
+
+
+        </CardBody>
+
+        <CardFooter>
+          <div className="flex flex-col w-full items-end px-10">
+            <Button>
+              Comment <CommentIcon fill="#000" width={30} height={30} size={30} />
+            </Button>
+
           </div>
-        </div>
-      </div>
+        </CardFooter>
+      </Card>
+
+
+      <Card>
+        <CardHeader>
+          <div className="p-6 w-full">
+            <Textarea
+              label="When have you reached product market fit?"
+              labelPlacement="outside"
+
+              className="max-w-full"
+            />
+          </div>
+
+        </CardHeader>
+        <CardBody>
+
+          <div className="py-3 pl-10">
+            <StepCard expanded={false} indicator styles="" />
+
+
+            <StepCard expanded indicator styles="relative  left-16 w-[calc(100%-64px)]" />
+
+
+
+            <StepCard expanded={false} indicator styles="relative left-32 w-[calc(100%-128px)]" />
+
+
+
+            <StepCard expanded={false} indicator={false} styles="relative left-48 w-[calc(100%-192px)]" />
+          </div>
+
+
+
+        </CardBody>
+
+        <CardFooter>
+          <div className="flex flex-col w-full items-end px-10 pb-4">
+            <Button>
+              Comment <CommentIcon fill="#000" width={30} height={30} size={30} />
+            </Button>
+
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
